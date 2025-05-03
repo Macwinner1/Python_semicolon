36c2b631c74bb84f3dd37c27c6c4dfc91bb8d3d5
Macwinner1 <ifechukwuw@gmail.com>
2025-05-03 04:11:27 +0100

    Arthmetic,y

---
+++ Arthmetic.py
@@ -0,0 +1,9 @@
+number1 = 27.5;
+number2 = 2; 
+
+print("This is the sum value of the above variable: ", number1 + number2)
+print("This is the sub value of the above variable: ", number1 - number2)
+print("This is the multiple value of the above variable: ", number1 * number2)
+print("This is the divide value of the above variable: ", number1 / number2)
+print("This is the multiple value of the above variable: ", number1 // number2)
+print("This is the exponential value of the above variable: ", number1 ** number2)
