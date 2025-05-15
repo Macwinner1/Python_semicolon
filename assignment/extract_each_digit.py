ca3a405b4094531804effd684195b6fc777756ab
Macwinner1 <ifechukwuw@gmail.com>
2025-05-16 00:17:00 +0100

    extract numbers and sum them up

---
+++ assignment/extract_each_digit.py
@@ -0,0 +1,10 @@
+numbers = (input("Kindly enter any number from 1 to 10000: "))
+
+length = len(numbers)
+numbers = int(numbers)
+value = 0
+for index in range(length):
+	value += numbers % 10
+	numbers = numbers // 10
+
+print(f"This is total sum of this above nupy mber {value}")
