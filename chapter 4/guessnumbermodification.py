e1c1803f47b1a200131043a12fc42092cba641a4
Macwinner1 <ifechukwuw@gmail.com>
2025-05-23 07:47:52 +0100

    guess number game

---
+++ chapter 4/guessnumbermodification.py
@@ -0,0 +1,29 @@
+import random
+
+count = True
+number = random.randint(1, 1000)
+guess_count = 0
+while count:
+	guess = int(input("Guess my number between 1 and 1000 with the fewest guesses:"))
+	if guess == number:
+		print("Congratulations. You guessed the number!")
+		play_again = int(input("Do you want to play again? press(1) for Yes and press(2) for Exit:"))
+		number = random.randint(1, 1000)
+		guess_count = 0
+		
+		if play_again == 1:
+			count = True
+		if play_again == 2:
+			count = False	
+	elif guess > number:
+		print("Too high. Try again")
+	elif guess < number:
+		print("Too low. Try again")
+	guess_count = guess_count + 1
+	
+	if guess_count < 10:
+		print("You should be able to do better!")
+	if guess_count > 10:
+		print("Either you know the secret or you got lucky!")	
+	
+	
